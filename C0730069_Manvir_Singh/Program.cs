c29259714aab7989f09852eb78a458e6af4952f8
visualstudio <visualstudio@122-PC06>
2019-01-18 21:27:02 -0500

    my third commit

--- C0730069_Manvir_Singh/Program.cs
+++ C0730069_Manvir_Singh/Program.cs
@@ -10,6 +10,25 @@ namespace C0730069_Manvir_Singh
     {
         static void Main(string[] args)
         {
+            StudentInformation _Mary = new StudentInformation("Marry", 3.8, 20);
+            StudentInformation _John = new StudentInformation("John", 3.8, 20);
+            StudentInformation _Mark = new StudentInformation("Mark", 3.8, 20);
+            StudentInformation _Steve = new StudentInformation("Steve", 3.8, 20);
+            StudentInformation _Cindy = new StudentInformation("Cindy", 3.8, 20);
+        }
+    }
+    class StudentInformation
+    {
+        string Name;
+        string StudentNumber;
+        double GPA;
+        int Attendance;
+        public StudentInformation(string aName, double cGPA, int dAttendance)
+        {
+            Name = aName;
+            StudentNumber = bStudentNumber;
+            GPA = cGPA;
+            Attendance = dAttendance;
         }
     }
 }
